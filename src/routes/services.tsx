9d874ebb0b2f03ef5c6433ac6e5848c0eda898fe
Manul Thanura <manulkarunarathna@gmail.com>
2025-02-07 10:49:06 +0530

    Add some routes to the application

---
+++ src/routes/services.tsx
@@ -0,0 +1,14 @@
+import { Container } from "@/components/container";
+
+const ServicesPage = () => {
+  return (
+    <div className="flex-col w-full pb-24">
+      <Container>
+        <div className="flex flex-wrap items-center justify-between -mx-4">
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default ServicesPage;
